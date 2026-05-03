2e560152ddee43f2973f1b20623cb1a5d3850010
dindarizkies <dindarizkies@gmail.com>
2026-05-03 23:17:03 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 10000;
+const PORT = 3000;
 
 // Set up multer for in-memory file storage
 const storage = multer.memoryStorage();
